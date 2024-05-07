2fe4ddbad92786dd37b91830d50397e0f1fbd21a
홍성민 <79087697+hongseungmn@users.noreply.github.com>
2024-05-07 17:33:17 +0900

    0507_커밋 정리

--- YULO/model_test/YOLOpose_video_2.py
+++ YULO/model_test/YOLOpose_video_2.py
@@ -7,10 +7,13 @@ from ultralytics import YOLO
 
 from fastapi.middleware.cors import CORSMiddleware
 
+import threading, time
+from functools import partial
+from datetime import datetime
+
 # FastAPI 앱 인스턴스 생성
 app = FastAPI()
-model = YOLO('yolov8n.pt')
-model.classes = [0]
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -19,40 +22,53 @@ app.add_middleware(
     allow_headers=["*"],  # 허용할 헤더 지정
 )
 
-def get_stream_video():
-    # camera 정의
-    cam = cv2.VideoCapture(0)
+def get_stream_video(model,count,label):
+  global detected_count  # 전역 변수로 카운트 저장
+  # camera 정의
+  cam = cv2.VideoCapture(0)
 
+  try:
     while True:
-        # 카메라 값 불러오기
-        success, frame = cam.read()
+      # 카메라 값 불러오기
+      success, frame = cam.read()
 
-        if not success:
-            break
+      if not success:
+        break
+      else:
+        results = model(frame,verbose=False, classes=[0])
+        detected_count = sum(len(r.boxes.cls) for r in results)
+        ret, buffer = cv2.imencode('.jpg', results[0].plot())
+        if ret:
+          frame = buffer.tobytes()
+          yield (b'--frame\r\n'
+            b'Content-Type: image/jpeg\r\n\r\n' +
+            frame + b'\r\n')
         else:
-            results = model(frame,verbose=False, classes=[0])
-            names = model.names
-            count = 0
-            for r in results:
-                for c in r.boxes.cls:
-                    count+= 1
-                    print(names[int(c)])
-                    
-            print(count)
-            ret, buffer = cv2.imencode('.jpg', results[0].plot())
-            if ret:
-              frame = buffer.tobytes()
-              yield (b'--frame\r\n'
-                  b'Content-Type: image/jpeg\r\n\r\n' +
-                  frame + b'\r\n')
-            else:
-            # yield로 하나씩 넘겨준다.
-              frame = buffer.tobytes()
-              yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
-                bytearray(frame) + b'\r\n')
-        
+        # yield로 하나씩 넘겨준다.
+          frame = buffer.tobytes()
+          yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
+            bytearray(frame) + b'\r\n')
+  finally:
+    cam.release() # 카메라 자원 해제
 
+@app.get("/count")
+def get_count():
+  data = generate_data()  # 샘플 데이터 생성
+  return data
 
+# 샘플 데이터 생성
+def generate_data():
+  global detected_count
+  now = datetime.now()
+  formatted_time = now.strftime("%H:%M:%S")
+  
+  # 데이터 예시: 시간, pv, amt
+  data = {
+    "dateTime": formatted_time,
+    "pv": detected_count,  # PV값 예시
+  }
+
+  return [data]
 
 
 @app.get('/get_url')
@@ -62,11 +78,13 @@ def get_url():
   }
   
   
-def video_streaming():
-  return get_stream_video()
+def video_streaming(count, label):
+  model = YOLO('yolov8n.pt')
+  model.classes = [label]
+  return get_stream_video(model,count, label)
   
 @app.get('/video')
-def main():
+def main(count: int, label: str):
     #StringResponse 함수를 return하고,
     #인자로 OpenCV에서 가져온 "바이트"이미지와 type을 명시
-    return StreamingResponse(video_streaming(), media_type="multipart/x-mixed-replace; boundary=frame")
+    return StreamingResponse(video_streaming(count, label), media_type="multipart/x-mixed-replace; boundary=frame")
